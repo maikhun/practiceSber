bfacef4ea4f0e6fd7c1e0f5a73e63369510b441b
Maryushkin Anton <62674062+maikhun@users.noreply.github.com>
2023-04-24 17:39:25 +0300

    Добавление файлов проекта CreditPayment

---
+++ credit_payment/src/main/java/com/sbertech/credit_payment/controllers/MainController.java
@@ -0,0 +1,19 @@
+package com.sbertech.credit_payment.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String getAuthorization(){
+        return "authorization";
+    }
+    @GetMapping("/index")
+    public String getMainPage(){
+        return "index";
+    }
+}
